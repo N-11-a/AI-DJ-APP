42b6f5cbac38309d559e26190f78a18e296f3f00
N-11-a <74191248+N-11-a@users.noreply.github.com>
2020-11-09 18:55:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup(){
+ canvas= createCanvas(500, 500);
+ canvas.center();
+ play();
+}
+
+function draw(){
+
+}
+function play(){
+    console.log("Its working");
+}
+
