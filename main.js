9f43db567b19d2b8db8deb09bac16c7a087b8e7c
N-11-a <74191248+N-11-a@users.noreply.github.com>
2020-11-13 16:15:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ function preload(){
 function setup(){
  canvas= createCanvas(500, 500);
  canvas.center();
- play();
+ video= createCapture(VIDEO);
+ video.hide();
+
 }
 
 function draw(){
-
+image(video, 0, 0, 500, 500);
 }
 function play(){
     console.log("Its working");
